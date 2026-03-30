3b77aa837a02ebe25dfb03988930204228e0fef4
Elvr21 <elauvray2101pro@gmail.com>
2026-03-30 17:27:15 +0200

    fonctions request

---
+++ PHP/requests.php
@@ -0,0 +1,20 @@
+<?php
+
+header('Content-Type: application/json');
+
+function dbRequestTopics($db) {
+    $query = $db->query('SELECT * FROM topics ORDER BY created_at DESC');
+    return $query->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function dbRequestTopic($db, $id) {
+    $query = $db->prepare('SELECT * FROM topics WHERE id = :id');
+    $query->execute([':id' => $id]);
+    return $query->fetch(PDO::FETCH_ASSOC);
+}
+
+function dbRequestReplies($db, $topicId) {
+    $query = $db->prepare('SELECT * FROM replies WHERE topicId = :topicId ORDER BY created_at ASC');
+    $query->execute([':topicId' => $topicId]);
+    return $query->fetchAll(PDO::FETCH_ASSOC);
+}
