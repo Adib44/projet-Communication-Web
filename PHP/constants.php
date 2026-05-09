ad32ba44aca8759afef2b40dbdfe0c7a0592da33
Simon <binetsimon485@gmail.com>
2026-05-09 19:58:18 +0200

    Ajout ; dans constant.php

--- PHP/constants.php
+++ PHP/constants.php
@@ -4,7 +4,7 @@ $db_file = __DIR__ . '/../Database/database.db';
 try{
     $pdo = new PDO("sqlite:" . $db_file);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC)
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (\PDOException $e) {
     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
